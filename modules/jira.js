fd2a26504dc197b3cce9db9b5f397800f42f5064
linazhukov <pzhukov@peakactivity.com>
2023-01-17 16:48:48 -0500

    load jira token from github secrets

--- modules/jira.js
+++ modules/jira.js
@@ -1,7 +1,35 @@
-function addComment(ticket_id, comment){
-    console.log(ticket_id);
-    console.log(comment);
+
+const JiraApi = require('jira-client');
+const token = process.env.jira_token;
+
+function jiraClient (){
+    const jira = new JiraApi({
+        protocol: 'https',
+        host: 'peakactivity.atlassian.net',
+        username: 'pzhukov@peakactivity.com',
+        password: token,
+        apiVersion: '2',
+        strictSSL: true
+    });
+    console.log({token})
+
+    async function addComment(ticket_id, comment){
+        console.log(ticket_id);
+        console.log(comment);
+
+        try {
+            const issue = await jira.findIssue(ticket_id);
+            console.log(`Status: ${issue.fields.status.name}`);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return {addComment}
 }
 
 
-module.exports = {addComment}
+
+
+
+module.exports = jiraClient()
