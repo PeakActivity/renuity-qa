fd87a0808e0241563d6ba3605a2ab4569d21fbe9
linazhukov <pzhukov@peakactivity.com>
2023-01-21 16:29:04 -0500

    transition issue when done

--- index.js
+++ index.js
@@ -2,31 +2,33 @@ const core = require('@actions/core');
 const pack = require('./packageFile')
 
 function formatResults(ticket, results){
-    let PASS = true;
+    let passed = true;
     let message = ``;
     for(const i of results){
         message += `\n- ${i.title} : ${i.status}`
         if(i.error){
-            PASS  = false;
+            passed  = false;
             for(const e of i.error){
                 message += `\n\t- ${e}`
             }
         }
     }
 
-    let summary = `QA ${PASS ? `PASSED` : `FAILED`}`;
+    let summary = `QA ${passed ? `PASSED` : `FAILED`}`;
 
-    return summary+message;
+    const result = summary+message;
+    return {result, passed}
 }
 
 try {
 
     const ticket = process.env.ticket;
 
-    const {results} = pack();
+    const {result, passed} = pack();
 
-    const _results = formatResults(ticket, results)
+    const _results = formatResults(ticket, result)
     core.setOutput('results', _results)
+    core.setOutput('passed', passed)
     return _results;
 } catch (error) {
     core.setFailed(error.message);
